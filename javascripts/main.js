f9f65498627a81a34e3e02bba51033cc05644fbd
Bree Thomas 2012 <breethomas33@Brees-MacBook-Pro.local>
2013-01-24 11:24:22 -0700

    Layout tweaks for media queries

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,79 +1,77 @@
+(function () {
 
+  var carousel_list;
+  var slide;
+  var panels;
 
-(function() {
+  function initialize() {
+    carousel_list = $(".carousel-nav .carousel-nav-link");
+    slide = $(".carousel-slide");
+    panels = slide.find('.carousel-panel')
+    slide.width(panels.length * 100 + '%')
+    addListeners();
+    resized()
+  }
 
-	var carousel_list;
-	var slide;
-	var panels;
+  function addListeners() {
+    var carousel_buttons = carousel_list.find('a')
+    var section_links = $(".section-link")
 
-	function initialize() {
-		carousel_list = $(".carousel-nav .carousel-nav-link");
-		slide = $(".carousel-slide");
-		panels = slide.find('.carousel-panel')
-		slide.width(panels.length * 100 + '%')
-		addListeners();
-		resized()
-	}
+    carousel_buttons.on('click', carouselGallery)
 
-	function addListeners() {
-		var carousel_buttons = carousel_list.find('a')
-		var section_links = $(".section-link")
+    section_links.not('.horizontal').on('click', scrollTo)
 
-		carousel_buttons.on('click', carouselGallery)
+    section_links.on("mouseover", function (event) {
+      arrowSlide(event, "mouseover")
+    });
 
-		section_links.on('click', scrollTo)
-		
-		section_links.on("mouseover", function(event) {
-			arrowSlide(event, "mouseover")
-		});
+    section_links.on("mouseout", function (event) {
+      arrowSlide(event, "mouseout")
+    });
 
-		section_links.on("mouseout", function(event) {
-			arrowSlide(event, "mouseout")
-		});
+    $(window).on('resize', resized)
+  }
 
-		$(window).on('resize', resized)
-	}
+  function carouselGallery(event) {
+    event.preventDefault();
+    resized()
+    var li = $(this).closest('li')
+    carousel_list.removeClass('active')
+    li.addClass('active')
+    var slidePosition = -100 * $(this).data("index");
+    slide.css("margin-left", slidePosition + "%");
+  }
 
-	function carouselGallery(event) {
-		event.preventDefault();
-		resized()
-		var li = $(this).closest('li')
-		carousel_list.removeClass('active')
-		li.addClass('active')
-		var slidePosition = -100 * $(this).data("index");
-		slide.css("margin-left",slidePosition + "%");
-	}
+  function resized(event) {
+    var offset = Math.round(slide.width() / panels.length)
+    panels.width(offset)
+  }
 
-	function resized(event) {
-		var offset = Math.round(slide.width() / panels.length)
-		panels.width(offset)
-	}
+  function arrowSlide(event, state) {
+    var target = $(event.target) //cast it as a jquery element
+    var img = target.find('img')
 
-	function arrowSlide(event, state) {
-		var target = $(event.target) //cast it as a jquery element
-		var img = target.find('img')
+    if (!img.length) {
+      img = target;
+    }
 
-		if (!img.length) {
-			img = target;
-		}
+    if (state === "mouseover") {
+      img.addClass("over")
+    } else {
+      img.removeClass("over")
+    }
+  }
 
-		if (state === "mouseover") {
-			img.addClass("over")
-		  } else {
-		  	img.removeClass("over")
-		  }
-	}
+  function scrollTo(event) {
+    event.preventDefault()
+    var button = $(this)
+    var id = button.attr('href')
+    var element = $(id)
+    var y_pos = element.offset().top
+    $('html body').animate({scrollTop: y_pos}, 500)
+  }
 
-	function scrollTo(event) {
-		event.preventDefault()
-		var button = $(this)
-		var id = button.attr('href')
-		var element = $(id)
-		var y_pos = element.offset().top
-		$('html body').animate({scrollTop: y_pos}, 500)
-	}
-
-	initialize()
+  initialize()
 
 })()
 
